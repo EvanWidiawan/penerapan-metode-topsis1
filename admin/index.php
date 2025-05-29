c1be1b302e1d46a234fcaf94525e0909f1d31a14
EvanWidiawan <saputevan26@gmail.com>
2025-05-29 16:18:03 +0700

    materi topsis

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,36 @@
 <?php include 'header.php'?>
 
-</style>
-
 <div class="container">
     <center>
-        <h3>
-            <b>PENERAPAN METODE TOPSIS</b>
-        </h3>
+        <h3><b>PENERAPAN METODE TOPSIS</b></h3>
     </center>
+
+    <br>
+
+    <div class="card">
+        <div class="card-body">
+            <h4><b>Pengertian Metode TOPSIS</b></h4>
+            <p>
+                TOPSIS (Technique for Order of Preference by Similarity to Ideal Solution) adalah salah satu metode pengambilan keputusan multikriteria yang menggunakan prinsip bahwa alternatif terpilih harus memiliki jarak terdekat dari solusi ideal positif (terbaik) dan jarak terjauh dari solusi ideal negatif (terburuk).
+            </p>
+
+            <h4><b>Langkah-langkah dalam Metode TOPSIS:</b></h4>
+            <ol>
+                <li>Membuat matriks keputusan berdasarkan kriteria dan alternatif.</li>
+                <li>Normalisasi matriks keputusan.</li>
+                <li>Mengalikan hasil normalisasi dengan bobot kriteria.</li>
+                <li>Menentukan solusi ideal positif dan negatif.</li>
+                <li>Menghitung jarak setiap alternatif ke solusi ideal positif dan negatif.</li>
+                <li>Menghitung nilai preferensi untuk setiap alternatif.</li>
+                <li>Menentukan peringkat alternatif berdasarkan nilai preferensi.</li>
+            </ol>
+
+            <h4><b>Keunggulan TOPSIS:</b></h4>
+            <ul>
+                <li>Proses perhitungan jelas dan logis.</li>
+                <li>Mempertimbangkan jarak terhadap solusi ideal.</li>
+                <li>Cocok digunakan dalam berbagai jenis pengambilan keputusan.</li>
+            </ul>
+        </div>
+    </div>
 </div>
